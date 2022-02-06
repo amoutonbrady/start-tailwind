cd2b5b6d85a228e49f5cd84687823dabb9c90c2f
Alexandre Mouton-Brady <amoutonbrady@gmail.com>
2022-02-06 17:34:54 +0100

    :wrench: Setup netlify?

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
+import netlify from "solid-start-netlify";
 
 export default defineConfig({
-  plugins: [solid()]
+  plugins: [solid({ adapter: netlify() })],
 });
